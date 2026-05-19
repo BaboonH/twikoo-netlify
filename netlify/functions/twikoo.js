8008b57c878d9b89a121aa3f64897963d79074c6
huangshu <995574522@qq.com>
2026-05-19 14:40:01 +0800

    跨域问题

--- netlify/functions/twikoo.js
+++ netlify/functions/twikoo.js
@@ -1,1 +1,29 @@
-exports.handler = require('twikoo-netlify').handler
+const twikoo = require('twikoo-netlify')
+
+exports.handler = async (event, context) => {
+  // 处理 OPTIONS 预检请求
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      },
+      body: '',
+    }
+  }
+  
+  const response = await twikoo.handler(event, context)
+  
+  // 添加 CORS 头到响应中
+  return {
+    ...response,
+    headers: {
+      ...response.headers,
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    },
+  }
+}
